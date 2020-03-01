1bcf63988452a1f066c8190cd3f2f2d30cdea945
LiSongPeng <1442364096@qq.com>
2020-03-01 21:55:29 +0800

    修改代码

--- src/main/java/com/learn/io/Client.java
+++ src/main/java/com/learn/io/Client.java
@@ -3,17 +3,53 @@ package com.learn.io;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 
 public class Client {
+    public static void doWrite(SocketChannel socketChannel) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(11);
+        byteBuffer.put("hello world".getBytes());
+        byteBuffer.flip();
+        try {
+            socketChannel.write(byteBuffer);
+            socketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         SocketChannel socketChannel = SocketChannel.open();
-        if (socketChannel.connect(new InetSocketAddress("localhost", 8888))) {
-            ByteBuffer byteBuffer = ByteBuffer.allocate(48);
-            byteBuffer.clear();
-            byteBuffer.put("hello world".getBytes());
-            byteBuffer.flip();
-            socketChannel.write(byteBuffer);
+        socketChannel.configureBlocking(false);
+        Selector selector = Selector.open();
+        boolean connected = socketChannel.connect(new InetSocketAddress("localhost", 8888));
+        if (!connected) {
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+        } else {
+            doWrite(socketChannel);
+            selector.close();
+            return;
+        }
+        while (true) {
+            if (selector.select() == 0) {
+                continue;
+            }
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = iterator.next();
+                iterator.remove();
+                SocketChannel channel = (SocketChannel) selectionKey.channel();
+                if (selectionKey.isConnectable()) {
+                    System.out.println("can connect");
+                    channel.finishConnect();
+                    doWrite(socketChannel);
+                    selector.close();
+                    return;
+                }
+            }
         }
     }
 }
--- src/main/java/com/learn/io/NIO.java
+++ src/main/java/com/learn/io/NIO.java
@@ -7,8 +7,7 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class NIO {
@@ -16,11 +15,6 @@ public class NIO {
     public static void main(String[] args) {
         ServerSocketChannelThread serverSocketChannelThread = new ServerSocketChannelThread();
         serverSocketChannelThread.startRunning();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("please input stop to stop!");
-        if (scanner.next() == "stop") {
-            serverSocketChannelThread.stopRunning();
-        }
     }
 }
 
@@ -32,48 +26,47 @@ class ServerSocketChannelThread extends Thread {
     public void run() {
         try {
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-            serverSocketChannel.configureBlocking(false);
             serverSocketChannel.bind(new InetSocketAddress("localhost", PORT));
+            serverSocketChannel.configureBlocking(false);
             Selector selector = Selector.open();
-            List<SocketChannel> socketChannelList = new LinkedList<>();
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
             while (running) {
-                selector.select(selectionKey -> {
-                    if (selectionKey.isAcceptable()) {
-                        try {
-                            SocketChannel socketChannel = ((ServerSocketChannel) selectionKey.channel()).accept();
-                            if (socketChannel != null) {
-                                System.out.println("accepted");
-                                socketChannel.configureBlocking(false);
-                                socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                if (selector.select() > 0) {
+                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey selectionKey = iterator.next();
+                        iterator.remove();
+                        if (selectionKey.isAcceptable()) {
+                            try {
+                                SocketChannel socketChannel = ((ServerSocketChannel) selectionKey.channel()).accept();
+                                if (socketChannel != null) {
+                                    System.out.println("accepted");
+                                    socketChannel.configureBlocking(false);
+                                    socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
                             }
-                        } catch (IOException e) {
-                            e.printStackTrace();
+                        }
+                        if (selectionKey.isReadable()) {
+                            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+                            ByteBuffer byteBuffer = (ByteBuffer) selectionKey.attachment();
+                            try {
+                                int readied = socketChannel.read(byteBuffer);
+                                if (readied == -1) {
+                                    socketChannel.close();
+                                }
+                                if (readied > 0) {
+                                    System.out.println("received:" + new String(byteBuffer.array(), 0, byteBuffer.limit()));
+                                    byteBuffer.clear();
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
-                    if (selectionKey.isReadable()) {
-                        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-                        ByteBuffer byteBuffer = (ByteBuffer) selectionKey.attachment();
-                        byteBuffer.clear();
-                        byteBuffer.flip();
-                        try {
-                            int readied = socketChannel.read(byteBuffer);
-                            if (readied == -1) {
-                                socketChannel.close();
-                            }
-                            if (readied > 0) {
-                                System.out.println("received:" + new String(byteBuffer.array(), 0, byteBuffer.limit()));
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+                }
             }
-            for (SocketChannel socketChannel : socketChannelList) {
-                socketChannel.close();
-            }
-            serverSocketChannel.close();
             selector.close();
         } catch (IOException e) {
             e.printStackTrace();
